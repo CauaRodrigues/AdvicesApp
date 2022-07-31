26437670b7dd95417830ecac5e55d88faf9cafa7
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-07-31 19:49:43 -0300

    responsive

--- src/components/BoxAdvice.jsx
+++ src/components/BoxAdvice.jsx
@@ -4,14 +4,13 @@ import "../styles/BoxAdvice.css";
 
 const BoxAdvice = () => {
 	let id = 117;
+	let advice =
+		"It is easy to sit up and take notice, what's difficult is getting up and taking action";
 	return (
 		<div className="box_advice">
 			<span className="advice_number">advice #{id}</span>
 
-			<span className="advice">
-				"It is easy to sit up and take notice, what's difficult is getting up
-				and taking action."
-			</span>
+			<span className="advice">"{advice}"</span>
 
 			<img className="divider_box" src={DividerDesktop} alt="Divider" />
 		</div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import BoxAdvice from "../components/BoxAdvice";
-import IconDice from "../assets/icon-dice.svg";
 import "../styles/Home.css";
+import IconDice from "../assets/icon-dice.svg";
 
 const Home = () => {
 	return (
 		<main>
-			<BoxAdvice />
+			<div className="container_boxAdvice">
+				<div className="row-box">
+					<BoxAdvice />
+				</div>
 
-			<div className="circle_button">
-				<img src={IconDice} alt="Button Advice" />
+				<div className="row-button">
+					<div className="circle_button">
+						<img src={IconDice} alt="Button Advice" />
+					</div>
+				</div>
 			</div>
 		</main>
 	);
