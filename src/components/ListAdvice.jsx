8cc0d2c319b199fa5ff8bcfbed564613648f9be6
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-09-03 00:37:22 -0300

    search advice for the api -> OK

--- src/components/ListAdvice.jsx
+++ src/components/ListAdvice.jsx
@@ -1,19 +1,51 @@
+import axios from "axios";
 import React from "react";
-import data from "../data.json";
+import { useState } from "react";
+import { useEffect } from "react";
+// import data from "../data.json";
 import "../styles/ListAdvice.css";
 
 const ListAdvice = ({ input }) => {
-	const filteredData = data.filter((el) =>
-		!input ? el : el.text.toLowerCase().includes(input)
-	);
+	// // const filteredData = data.filter((el) =>
+	// // 	!input
+	// // 		? el
+	// // 		: el.text.toLowerCase().includes(input) ||
+	// // 		  el.id.toString().includes(input)
+	// // );
+
+	const [adviceList, setAdviceList] = useState([]);
+
+	useEffect(() => {
+		if (input) {
+			axios
+				.get(`https://api.adviceslip.com/advice/search/${input}`)
+				.then(({ data }) => {
+					if (data) {
+						setAdviceList(data.slips);
+					} else {
+						setAdviceList([]);
+					}
+				});
+		} else {
+			setAdviceList([]);
+		}
+	}, [input]);
 
 	return (
 		<ul className="advices-list">
-			{filteredData.map((item) => (
-				<li className="advice" key={item.id}>
-					{item.text}
-				</li>
-			))}
+			{adviceList
+				? adviceList.map(({ id, advice }) => (
+						<li className="advice" key={id}>
+							<div className="box-id box">
+								<span className="id">{id}</span>
+							</div>
+
+							<div className="box-text box">
+								<span className="text">{advice}</span>
+							</div>
+						</li>
+				  ))
+				: null}
 		</ul>
 	);
 };
